102b1fdf858a4832cfa8798e3af54c2792b8b9c3
SharonL-maker <slandes92@gamil.com>
2020-08-11 09:11:22 +0300

    Path creation changes, better to use os.path.join

--- Auto_move_files.py
+++ Auto_move_files.py
@@ -1,5 +1,5 @@
 
-from watchdog.observers import observer
+from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 import os
@@ -11,13 +11,13 @@ class MyHandler(FileSystemEventHandler):
 
     def on_modified(self, event):
         for filename in os.listdir(folder_to_track):
-            src = folder_to_track + "/" + filename
-            new_destination = folder_destination + "/" + filename
+            src = os.path.join(folder_to_track, filename)
+            new_destination = os.path.join(folder_destination, filename)
             os.rename(src, new_destination)
 
 
 folder_to_track = r"C:\Users\sharo\Downloads\PopcornTime"
-folder_destination = "D:\Videos&Movies"
+folder_destination = r"C:\Users\sharo\Videos"
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive = True)
